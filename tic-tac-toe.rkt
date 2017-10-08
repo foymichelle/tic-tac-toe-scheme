d95270062143d227bcbd3a30ca5390663e7ddfcc
Michelle Foy <michelle.foy@cenx.com>
2017-10-08 09:20:51 -0400

    Working version of tic-tac-toe

--- tic-tac-toe.rkt
+++ tic-tac-toe.rkt
@@ -1,5 +1,115 @@
 #lang racket
 
-(define (tic-tac-toe M)
-  M)
-  
+;; Display board fns
+(define init-board 
+  '((1 2 3)
+    (4 5 6)
+    (7 8 9)))
+
+(define (char->str char)
+  (cond
+    ((number? char) (number->string char))
+    ((symbol? char) (symbol->string char))
+    (else "")))
+
+(define (display-cell cell)
+  (string-append " " (char->str cell) " "))
+
+(define (display-row row)
+  (string-append* (add-between (map display-cell row) "|")))
+
+(define (display-board board)
+  (begin
+    (newline)
+    (display (string-append* (add-between (map display-row board) "\n---+---+---\n")))
+    (newline)))
+
+;; Make a move on board fns
+(define (replace-row token cell row)
+  (cond
+    ((null? row) '())
+    ((eq? cell (car row)) (cons token (cdr row)))
+    (else (cons (car row) (replace-row token cell (cdr row))))))
+
+(define (make-move token cell board)
+  (map (lambda (row) (replace-row token cell row)) board))
+
+;; Player interaction fns
+(define (free-cell? cell board)
+  (let ((member-space (lambda (row) (member cell row))))
+    (ormap member-space board)))
+
+(define (select-cell)
+  (begin
+    (display "Select a cell (1-9): ")
+    (string->number (read-line))))
+
+(define (x-play board)
+  (let ((answer (select-cell)))
+    (make-move 'x answer board)))
+
+;; Computer play fns
+(define (free-cells board)
+  (let ((board-list (flatten board)))
+    (if (null? board-list)
+      '()
+      (if (number? (car board-list))
+          (cons (car board-list) (free-cells (cdr board-list)))
+          (free-cells (cdr board-list))))))
+
+(define (choose-random board)
+  (let ((cells (free-cells board)))
+    (list-ref cells (random (length cells)))))
+    
+(define (next-move-winner token board)
+  (filter (lambda (pos) (if (winner? (make-move token pos board)) #t #f)) (free-cells board)))
+
+(define (o-play board)
+  (let ((check-x-win (next-move-winner 'x board))
+        (check-o-win (next-move-winner 'o board)))
+    (cond
+      ((not (empty? check-o-win)) (make-move 'o (car check-o-win) board))
+      ((not (empty? check-x-win)) (make-move 'o (car check-x-win) board))
+      (else (make-move 'o (choose-random board) board)))))
+
+;; Check winner fns
+(define (all-equal? seq)
+  (foldl (lambda (x y) (if (eq? x y) x #f)) (car seq) (cdr seq))) 
+
+(define (winner? board)
+  (or
+   (all-equal? (first board))
+   (all-equal? (second board))
+   (all-equal? (third board))
+   (all-equal? (map first board))
+   (all-equal? (map second board))
+   (all-equal? (map third board))
+   (all-equal? (list (first (first board))
+                           (second (second board)) 
+                           (third (third board))))
+   (all-equal? (list (third (first board)) 
+                           (second (second board)) 
+                           (first (third board))))))
+
+;; Check for draw
+(define (draw? board)
+  (empty? (free-cells board)))
+
+;; Main fn
+(define (start)
+  (begin
+    (display "Welcome to Tic Tac Toe! You are player X.")
+    (play init-board)))
+
+(define (play board)
+  (begin
+    (display-board board)
+    (let ((x-board (x-play board)))
+      (cond
+        ((winner? x-board) (display "X wins!"))
+        ((draw? x-board) (display "It's a draw!"))
+        (else (let ((o-board (o-play x-board)))
+                (cond
+                  ((winner? o-board) (display "O wins!"))
+                  ((draw? o-board) (display "It's a draw!"))
+                  (else (play o-board)))))))))
